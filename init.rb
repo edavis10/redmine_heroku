642db2f17c2d4cdd1abf30306559c00d15fb67e4
Eric Davis <edavis@littlestreamsoftware.com>
2009-12-22 15:43:32 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_heroku do
+  name 'Redmine Heroku plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
